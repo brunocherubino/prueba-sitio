d9c524af936d3c1ed2a1832efe36d555672636e0
brunocherubino <80709302+brunocherubino@users.noreply.github.com>
2023-04-27 22:28:49 -0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,50 +1,18 @@
 <?php
-// Importar la clase PHPMailer
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Configurar la dirección de correo electrónico del destinatario
+  $destinatario = "destinatario@ejemplo.com";
 
-// Cargar la clase PHPMailer
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+  // Obtener los valores del formulario
+  $asunto = $_POST["subject"];
+  $cuerpo = $_POST["body"];
+  $headers = "From: " . $_POST["from"];
 
-// Obtener los datos del formulario
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
-
-// Configurar el mensaje del correo electrónico
-$asunto = "Nuevo mensaje de " . $nombre;
-$cuerpo = "Nombre: " . $nombre . "\n" .
-          "Correo electrónico: " . $correo . "\n" .
-          "Número de teléfono: " . $telefono . "\n\n" .
-          "Mensaje:\n" . $mensaje;
-
-// Crear una instancia de PHPMailer
-$mail = new PHPMailer(true);
-
-try {
-    // Configurar el servidor SMTP
-    $mail->SMTPDebug = 0;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; // Cambiar por el servidor SMTP de tu proveedor de correo electrónico
-    $mail->SMTPAuth = true;
-    $mail->Username = 'Brunocherubinoo01@gmail.com'; // Cambiar por tu dirección de correo electrónico
-    $mail->Password = 'brunocomu01'; // Cambiar por tu contraseña
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-
-    // Configurar los destinatarios y el mensaje
-    $mail->setFrom($correo, $nombre);
-    $mail->addAddress('Brunocherubinoo01@gmail.com'); // Cambiar por la dirección de correo electrónico del destinatario
-    $mail->Subject = $asunto;
-    $mail->Body = $cuerpo;
-
-    // Enviar el mensaje
-    $mail->send();
-    echo '¡Mensaje enviado!';
-} catch (Exception $e) {
-    echo 'Error al enviar el mensaje: ' . $mail->ErrorInfo;
+  // Enviar el correo electrónico
+  if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+    echo "Correo electrónico enviado";
+  } else {
+    echo "Hubo un problema al enviar el correo electrónico";
+  }
 }
 ?>
